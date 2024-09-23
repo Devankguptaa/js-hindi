b373a544b8e4c713b3c8ca1946a3fba54478dafd
Praksah-ux <153495922+Praksah-ux@users.noreply.github.com>
2024-09-23 19:04:51 +0530

    done

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -52,7 +52,18 @@ anothername = "chaiaurcode"
 
 console.log(anothername)
 console.log(myYoutube)
+ 
 
+let user1={
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let user2=user1
+
+user2.email="hitesh@google.com"
+console.log(user1.email)
+console.log(user2.email)
 
 
 
